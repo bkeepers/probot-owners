8252074a9041b267b46d4f20efcb2f76e7a6991f
Lee Dohm <lee@lee-dohm.com>
2017-03-14 18:33:29 -0700

    Add basic test for OwnerNotifier.getOwners()

--- test/owner-notifier.js
+++ test/owner-notifier.js
@@ -3,31 +3,62 @@ const expect = require('expect');
 const OwnerNotifier = require('../lib/owner-notifier');
 
 describe('OwnerNotifier', () => {
-  let github;
   let event;
+  let github;
   let notifier;
-
-  beforeEach(() => {
-    github = expect.createSpy();
-    event = {};
-    notifier = new OwnerNotifier(github, event);
-  });
 
   describe('repo property', () => {
     beforeEach(() => {
-      event.payload = {
-        repository: {
-          name: 'bar',
-          owner: {
-            login: 'foo'
+      event = {
+        payload: {
+          repository: {
+            name: 'bar',
+            owner: {
+              login: 'foo'
+            }
           }
         }
       };
+
+      github = expect.createSpy();
+      notifier = new OwnerNotifier(github, event);
     });
 
     it('extracts the right information', () => {
       expect(notifier.repo).toMatch({owner: 'foo', repo: 'bar'});
       expect(github).toNotHaveBeenCalled();
+    });
+  });
+
+  describe('getOwners', () => {
+    beforeEach(() => {
+      event = {
+        payload: {
+          repository: {
+            name: 'bar',
+            owner: {
+              login: 'foo'
+            }
+          }
+        }
+      };
+
+      github = {
+        repos: {
+          getContent: expect.createSpy().andReturn(Promise.resolve({
+            content: new Buffer('manny\nmoe\njack').toString('base64')
+          }))
+        }
+      };
+
+      notifier = new OwnerNotifier(github, event);
+    });
+
+    it('returns an ownersFile object', async () => {
+      const ownersFile = await notifier.getOwners();
+
+      expect(ownersFile).toExist();
+      expect(ownersFile.for).toExist();
     });
   });
 });
