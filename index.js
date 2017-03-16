8f2bcb6d4852828ff6996491524eea34b0edf80a
Lee Dohm <lee-dohm@github.com>
2017-03-15 20:24:50 -0700

    Notify on pull_request.opened as well as synchronize

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const OwnerNotifier = require('./lib/owner-notifier');
 
 module.exports = robot => {
-  robot.on('pull_request.synchronize', async event => {
+  const notify = async function (event) {
     const github = await robot.auth(event.payload.installation.id);
     const notifier = new OwnerNotifier(github, event);
 
     return notifier.check();
-  });
+  }
+
+  robot.on('pull_request.opened', notify);
+  robot.on('pull_request.synchronize', notify);
 };
