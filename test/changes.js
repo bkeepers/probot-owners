eb66d0a98a1525242786540e17751b07708c1681
Bex Warner <bexmwarner@gmail.com>
2017-09-05 20:45:52 +0200

    Use standard for linting

--- test/changes.js
+++ test/changes.js
@@ -1,30 +1,30 @@
-const expect = require('expect');
+const expect = require('expect')
 
-const Changes = require('../lib/changes');
+const Changes = require('../lib/changes')
 
 describe('Changes', () => {
-  let changes;
-  let ownersFile;
-  let paths;
+  let changes
+  let ownersFile
+  let paths
 
   describe('owners property', () => {
     beforeEach(() => {
       ownersFile = {
         for: expect.createSpy().andReturn(['manny', 'moe', 'jack'])
-      };
+      }
 
-      paths = ['foo', 'bar', 'baz'];
+      paths = ['foo', 'bar', 'baz']
 
-      changes = new Changes(paths, ownersFile);
-    });
+      changes = new Changes(paths, ownersFile)
+    })
 
     it('returns the appropriate values', () => {
-      const owners = changes.owners;
+      const owners = changes.owners
 
-      expect(owners).toInclude('manny');
-      expect(owners).toInclude('moe');
-      expect(owners).toInclude('jack');
-      expect(owners.length).toEqual(3);
-    });
-  });
-});
+      expect(owners).toInclude('manny')
+      expect(owners).toInclude('moe')
+      expect(owners).toInclude('jack')
+      expect(owners.length).toEqual(3)
+    })
+  })
+})
--- test/owner-notifier.js
+++ test/owner-notifier.js
@@ -1,25 +1,25 @@
-const expect = require('expect');
+const expect = require('expect')
 
-const Changes = require('../lib/changes');
-const OwnerNotifier = require('../lib/owner-notifier');
+const Changes = require('../lib/changes')
+const OwnerNotifier = require('../lib/owner-notifier')
 
-function createComment(userType, body) {
+function createComment (userType, body) {
   return {
     body,
     user: {
       type: userType
     }
-  };
+  }
 }
 
 describe('OwnerNotifier', () => {
-  const BASE_SHA = '1234567890abcdef1234567890abcdef12345678';
-  const HEAD_SHA = '234567890abcdef1234567890abcdef123456789';
-  const ISSUE_NUMBER = 42;
+  const BASE_SHA = '1234567890abcdef1234567890abcdef12345678'
+  const HEAD_SHA = '234567890abcdef1234567890abcdef123456789'
+  const ISSUE_NUMBER = 42
 
-  let event;
-  let github;
-  let notifier;
+  let event
+  let github
+  let notifier
 
   beforeEach(() => {
     event = {
@@ -31,20 +31,20 @@ describe('OwnerNotifier', () => {
           }
         }
       }
-    };
-  });
+    }
+  })
 
   describe('repo property', () => {
     beforeEach(() => {
-      github = expect.createSpy();
-      notifier = new OwnerNotifier(github, event);
-    });
+      github = expect.createSpy()
+      notifier = new OwnerNotifier(github, event)
+    })
 
     it('extracts the right information', () => {
-      expect(notifier.repo).toMatch({owner: 'foo', repo: 'bar'});
-      expect(github).toNotHaveBeenCalled();
-    });
-  });
+      expect(notifier.repo).toMatch({owner: 'foo', repo: 'bar'})
+      expect(github).toNotHaveBeenCalled()
+    })
+  })
 
   describe('getOwners', () => {
     beforeEach(() => {
@@ -56,23 +56,23 @@ describe('OwnerNotifier', () => {
             }
           }))
         }
-      };
+      }
 
-      notifier = new OwnerNotifier(github, event);
-    });
+      notifier = new OwnerNotifier(github, event)
+    })
 
     it('returns an ownersFile object', async () => {
-      const ownersFile = await notifier.getOwners();
+      const ownersFile = await notifier.getOwners()
 
-      expect(ownersFile).toExist();
-      expect(ownersFile.for).toExist();
+      expect(ownersFile).toExist()
+      expect(ownersFile.for).toExist()
       expect(github.repos.getContent).toHaveBeenCalledWith({
         owner: 'foo',
         repo: 'bar',
         path: 'OWNERS'
-      });
-    });
-  });
+      })
+    })
+  })
 
   describe('getChanges', () => {
     beforeEach(() => {
@@ -83,7 +83,7 @@ describe('OwnerNotifier', () => {
         head: {
           sha: HEAD_SHA
         }
-      };
+      }
 
       github = {
         repos: {
@@ -105,60 +105,60 @@ describe('OwnerNotifier', () => {
             }
           }))
         }
-      };
+      }
 
-      notifier = new OwnerNotifier(github, event);
-    });
+      notifier = new OwnerNotifier(github, event)
+    })
 
     it('returns an appropriate Changes object', async () => {
-      const changes = await notifier.getChanges();
+      const changes = await notifier.getChanges()
 
-      expect(changes).toBeA(Changes);
-      expect(changes.paths).toInclude('wibble');
-      expect(changes.paths).toInclude('wobble');
-      expect(changes.paths.length).toEqual(2);
+      expect(changes).toBeA(Changes)
+      expect(changes.paths).toInclude('wibble')
+      expect(changes.paths).toInclude('wobble')
+      expect(changes.paths.length).toEqual(2)
 
       expect(github.repos.compareCommits).toHaveBeenCalledWith({
         owner: 'foo',
         repo: 'bar',
         base: BASE_SHA,
         head: HEAD_SHA
-      });
-    });
-  });
+      })
+    })
+  })
 
   describe('comment', () => {
     beforeEach(() => {
-      event.payload.number = ISSUE_NUMBER;
+      event.payload.number = ISSUE_NUMBER
 
       github = {
         issues: {
           createComment: expect.createSpy().andReturn(Promise.resolve())
         }
-      };
+      }
 
-      notifier = new OwnerNotifier(github, event);
-    });
+      notifier = new OwnerNotifier(github, event)
+    })
 
     it('returns successfully', async () => {
       await notifier.comment([
         '@manny',
         '@moe',
         '@jack'
-      ]);
+      ])
 
       expect(github.issues.createComment).toHaveBeenCalledWith({
         owner: 'foo',
         repo: 'bar',
         number: ISSUE_NUMBER,
         body: '/cc @manny @moe @jack'
-      });
-    });
-  });
+      })
+    })
+  })
 
   describe('getAlreadyPingedOwners', () => {
     beforeEach(() => {
-      event.payload.number = ISSUE_NUMBER;
+      event.payload.number = ISSUE_NUMBER
 
       github = {
         issues: {},
@@ -169,87 +169,87 @@ describe('OwnerNotifier', () => {
             }
           }))
         }
-      };
+      }
 
-      notifier = new OwnerNotifier(github, event);
-    });
+      notifier = new OwnerNotifier(github, event)
+    })
 
     it('recognizes a properly formed comment from a Bot user', async () => {
       github.issues.getComments = expect.createSpy().andReturn(Promise.resolve(
         {data: [createComment('Bot', '/cc @manny')]}
-    ));
+    ))
 
-      const pings = await notifier.getAlreadyPingedOwners();
+      const pings = await notifier.getAlreadyPingedOwners()
 
-      expect(pings.length).toEqual(1);
-      expect(pings).toInclude('@manny');
-    });
+      expect(pings.length).toEqual(1)
+      expect(pings).toInclude('@manny')
+    })
 
     it('recognizes multiple pings in a single comment from a Bot user', async () => {
       github.issues.getComments = expect.createSpy().andReturn(Promise.resolve(
         {data: [createComment('Bot', '/cc @manny @moe @jack')]}
-    ));
+    ))
 
-      const pings = await notifier.getAlreadyPingedOwners();
+      const pings = await notifier.getAlreadyPingedOwners()
 
-      expect(pings.length).toEqual(3);
-      expect(pings).toInclude('@manny');
-      expect(pings).toInclude('@moe');
-      expect(pings).toInclude('@jack');
-    });
+      expect(pings.length).toEqual(3)
+      expect(pings).toInclude('@manny')
+      expect(pings).toInclude('@moe')
+      expect(pings).toInclude('@jack')
+    })
 
     it('rejects pings from other user types', async () => {
       github.issues.getComments = expect.createSpy().andReturn(Promise.resolve(
         {data: [createComment('User', '/cc @manny @moe @jack')]}
-    ));
+    ))
 
-      const pings = await notifier.getAlreadyPingedOwners();
+      const pings = await notifier.getAlreadyPingedOwners()
 
-      expect(pings.length).toEqual(0);
-    });
+      expect(pings.length).toEqual(0)
+    })
 
     it('rejects pings that are not formatted correctly', async () => {
       github.issues.getComments = expect.createSpy().andReturn(Promise.resolve(
         {data: [createComment('User', '@manny @moe @jack')]}
-    ));
+    ))
 
-      const pings = await notifier.getAlreadyPingedOwners();
+      const pings = await notifier.getAlreadyPingedOwners()
 
-      expect(pings.length).toEqual(0);
-    });
+      expect(pings.length).toEqual(0)
+    })
 
     it('only includes one copy of any name', async () => {
       github.issues.getComments = expect.createSpy().andReturn(Promise.resolve(
         {data: [createComment('Bot', '/cc @manny @manny'), createComment('Bot', '/cc @manny')]}
-    ));
+    ))
 
-      const pings = await notifier.getAlreadyPingedOwners();
+      const pings = await notifier.getAlreadyPingedOwners()
 
-      expect(pings.length).toEqual(1);
-    });
-  });
+      expect(pings.length).toEqual(1)
+    })
+  })
 
   describe('getOwnersToPing', () => {
     beforeEach(() => {
-      event.payload.number = ISSUE_NUMBER;
+      event.payload.number = ISSUE_NUMBER
 
       github = {
         issues: {}
-      };
+      }
 
-      notifier = new OwnerNotifier(github, event);
-    });
+      notifier = new OwnerNotifier(github, event)
+    })
 
     it('returns only owners that have not been pinged', async () => {
       github.issues.getComments = expect.createSpy().andReturn(Promise.resolve(
         {data: [createComment('Bot', '/cc @manny')]}
-    ));
+    ))
 
-      const pings = await notifier.getOwnersToPing(['@manny', '@moe', '@jack']);
+      const pings = await notifier.getOwnersToPing(['@manny', '@moe', '@jack'])
 
-      expect(pings.length).toEqual(2);
-      expect(pings).toInclude('@moe');
-      expect(pings).toInclude('@jack');
-    });
-  });
-});
+      expect(pings.length).toEqual(2)
+      expect(pings).toInclude('@moe')
+      expect(pings).toInclude('@jack')
+    })
+  })
+})
